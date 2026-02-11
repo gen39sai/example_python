4cbfebcd1048221768404cce02cd31852cb84672
LinuC <linuc@localhost.localdomain>
2026-02-11 18:45:24 +0900

    ステップ150課題

---
+++ example.py
@@ -0,0 +1,22 @@
+
+print("hello world")
+
+def greet():
+    print("こんにちは")
+
+greet()
+
+def print_name(name):
+    print(f"私の名前は{name}です")
+
+print_name("げん")
+
+def get_greet():
+    return "おはようございます"
+
+print(get_greet())
+
+def add(a, b):
+    return a + b
+
+print(add(5,8))
